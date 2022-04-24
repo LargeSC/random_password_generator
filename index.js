b2a3e8fa739281a9eb62d9ea0df07a0597b63aa0
Large SC <98948674+LargeSC@users.noreply.github.com>
2022-04-24 13:15:25 +0200

    Initial file upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let passLen = document.getElementById("pass-len").value
+let passArray = []
+
+let lowerCase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+let upperCase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+let symbols=[".","?","!","_","@"]
+let numbers=[1,2,3,4,5,6,7,8,9,0]
+
+let data = [].concat(lowerCase,upperCase,symbols,numbers)
+
+// console.log(data)
+
+
+function generatePassword(){
+    passLen = document.getElementById("pass-len").value
+    passArray = []
+    for(let i =0;i<4;i++){
+        let password = "" 
+        for(let i=0; i<passLen;i++){
+            let randIndex = Math.floor(Math.random()*data.length);
+            password +=data[randIndex]
+        }
+        passArray.push(password)
+    }
+    renderPassHtml(passArray)
+}
+
+function renderPassHtml (array){
+    const passHtml = passArray.map(pass => {
+       return `<input type="text" class="password" onfocus="copyPass()" value="${pass}"></input>`
+    }).join('')
+    document.getElementById("pass-container").innerHTML = passHtml
+}
+
+
+function copyPass(){
+    const copyPass= document.activeElement.value
+    document.activeElement.select()
+    document.execCommand('copy');
+    setTimeout(() => {document.activeElement.value = "Copied"}, 250);
+    setTimeout(() => {document.activeElement.value = copyPass}, 600);
+}
